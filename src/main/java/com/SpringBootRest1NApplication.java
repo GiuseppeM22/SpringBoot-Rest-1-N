2dabd3b3b9deadda4a362b70650bc0f78ee3ef9a
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-11-27 18:05:34 +0100

    Scaffolding Project

---
+++ src/main/java/com/SpringBootRest1NApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRest1NApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRest1NApplication.class, args);
+	}
+
+}
